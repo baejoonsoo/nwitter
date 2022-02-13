d943fa5ce1dd154b3f15841d57fd8e0be3e9108f
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-14 01:20:51 +0900

    create Router.jsx & router-dom version upgrade

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import firebase from '../firebase';
+import AppRouter from './Router';
+
+console.log(firebase);
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
