80ec4bfbe94a475c428ec79f44ff197f221e1f12
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-13 22:41:00 +0900

    []

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-import "firebase/compat/storage";
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkWsYAjo3DpfYS7S_vI1L08SHI1yp5110",
-  authDomain: "nwitter-4b947.firebaseapp.com",
-  projectId: "nwitter-4b947",
-  storageBucket: "nwitter-4b947.appspot.com",
-  messagingSenderId: "903794381615",
-  appId: "1:903794381615:web:49c1cae1cc41465d8e47da",
+  apiKey: 'AIzaSyCkWsYAjo3DpfYS7S_vI1L08SHI1yp5110',
+  authDomain: 'nwitter-4b947.firebaseapp.com',
+  projectId: 'nwitter-4b947',
+  storageBucket: 'nwitter-4b947.appspot.com',
+  messagingSenderId: '903794381615',
+  appId: '1:903794381615:web:49c1cae1cc41465d8e47da',
 };
 
 export default firebase.initializeApp(firebaseConfig);
