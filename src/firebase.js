082b3ceb2a3e2f4a4f83da7e609e94148ad676b9
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-02-14 00:39:39 +0900

    firebase init

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCkWsYAjo3DpfYS7S_vI1L08SHI1yp5110',
-  authDomain: 'nwitter-4b947.firebaseapp.com',
-  projectId: 'nwitter-4b947',
-  storageBucket: 'nwitter-4b947.appspot.com',
-  messagingSenderId: '903794381615',
-  appId: '1:903794381615:web:49c1cae1cc41465d8e47da',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
